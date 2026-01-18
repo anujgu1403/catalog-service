0350b2562ea02bf6536a714db639b440c00feefa
Anuj Kumar <anujgupta1403@gmail.com>
2026-01-18 22:44:15 +0530

    Added lombok

--- src/main/java/com/retail/catalog/application/model/Category.java
+++ src/main/java/com/retail/catalog/application/model/Category.java
@@ -1,29 +1,15 @@
 package com.retail.catalog.application.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Data
+@AllArgsConstructor
 public class Category {
     private Integer categoryId;
     private String name;
-
-    public Category() {}
-
-    public Category(Integer categoryId, String name) {
-        this.categoryId = categoryId;
-        this.name = name;
-    }
-
-    public Integer getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Integer categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- src/main/java/com/retail/catalog/domain/CategoryModel.java
+++ src/main/java/com/retail/catalog/domain/CategoryModel.java
@@ -1,29 +1,16 @@
 package com.retail.catalog.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Data
+@AllArgsConstructor
 public class CategoryModel {
     private Integer categoryId;
     private String name;
 
-    public CategoryModel() {}
-
-    public CategoryModel(Integer categoryId, String name) {
-        this.categoryId = categoryId;
-        this.name = name;
-    }
-
-    public Integer getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Integer categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
